a9af05f4fea21d698535c1864f3fc117824fe58d
chendengfeng0820 <18790120405@163.com>
2020-06-12 17:01:22 +0800

    修改

---
+++ README.md
@@ -0,0 +1,2 @@
+# cupboard
+柜子
--- register-login/src/main/java/com/info/register_login/controller/Register.java
+++ register-login/src/main/java/com/info/register_login/controller/Register.java
@@ -1,9 +1,12 @@
 package com.info.register_login.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,6 +20,13 @@ public class Register {
 
 	@Autowired
 	private RedisTemplate redisTemplate;
+
+
+	@RequestMapping("/")
+	public String register(@RequestBody JSONObject jsonObject){
+
+		return null;
+	}
 
 
 
