9bc0fa775152049229413950713097e928e8cf70
chendengfeng0820 <18790120405@163.com>
2020-06-18 01:52:38 +0800

    redis缓存

--- register-login/src/main/java/com/info/register_login/service/Impl/UserServiceImpl.java
+++ register-login/src/main/java/com/info/register_login/service/Impl/UserServiceImpl.java
@@ -3,10 +3,13 @@ package com.info.register_login.service.Impl;
 import com.info.register_login.mapper.UserMapper;
 import com.info.register_login.pojo.User;
 import com.info.register_login.service.IUserService;
+import com.info.register_login.utils.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName UserServiceImpl
@@ -17,46 +20,114 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements IUserService {
 
-	@Autowired
-	private UserMapper userMapper;
+    @Autowired
+    private UserMapper userMapper;
 
-	@Override
-	public void saveUser(User user) {
-		userMapper.saveUser(user);
-	}
+    @Autowired
+    private RedisUtil redisUtil;
 
-	@Override
-	public void deleteUserById(Integer user_id) {
-		userMapper.deleteUserById(user_id);
-	}
+    @Override
+    public void saveUser(User user) {
 
-	@Override
-	public void deleteUserByTelephone(String user_telephone) {
-		userMapper.deleteUserByTelephone(user_telephone);
-	}
+        //添加到redis中
+        redisUtil.hset(user.getUser_telephone(), "user_id", user.getUser_id());
+        redisUtil.hset(user.getUser_telephone(), "user_username", user.getUser_username());
+        redisUtil.hset(user.getUser_telephone(), "user_telephone", user.getUser_telephone());
+        redisUtil.hset(user.getUser_telephone(), "user_password", user.getUser_password());
+        redisUtil.hset(user.getUser_telephone(), "user_faceinfo", user.getUser_faceinfo());
+        redisUtil.hset(user.getUser_telephone(), "user_createtime", user.getUser_createtime());
 
-	@Override
-	public void updateUser(User user) {
-		userMapper.updateUser(user);
-	}
+        //添加到mysql中
+        userMapper.saveUser(user);
+    }
 
-	@Override
-	public User findById(Integer user_id) {
-		return userMapper.findById(user_id);
-	}
+    @Override
+    public void deleteUserById(Integer user_id) {
 
-	@Override
-	public User findByTelephone(Integer user_telephone) {
-		return userMapper.findByTelephone(user_telephone);
-	}
+        userMapper.deleteUserById(user_id);
+    }
 
-	@Override
-	public List<User> findAll() {
-		return userMapper.findAll();
-	}
+    @Override
+    public void deleteUserByTelephone(String user_telephone) {
 
-	@Override
-	public User showInformation(String user_telephone) {
-		return userMapper.showInformation(user_telephone);
-	}
+        //从redis中删除
+        redisUtil.hdel(user_telephone);
+
+        //从mysql中删除
+        userMapper.deleteUserByTelephone(user_telephone);
+    }
+
+
+    /**
+     * 更新用户策略：先更新数据表，成功之后，删除原来的缓存，再更新缓存
+     */
+    @Override
+    public void updateUser(User user) {
+
+        //修改mysql数据
+        userMapper.updateUser(user);
+
+        //删除redis中数据
+        if (redisUtil.hashKey(user.getUser_telephone())) {
+            redisUtil.hdel(user.getUser_telephone());
+        }
+
+        //添加到redis中
+        redisUtil.hset(user.getUser_telephone(), "user_id", user.getUser_id());
+        redisUtil.hset(user.getUser_telephone(), "user_username", user.getUser_username());
+        redisUtil.hset(user.getUser_telephone(), "user_telephone", user.getUser_telephone());
+        redisUtil.hset(user.getUser_telephone(), "user_password", user.getUser_password());
+        redisUtil.hset(user.getUser_telephone(), "user_faceinfo", user.getUser_faceinfo());
+        redisUtil.hset(user.getUser_telephone(), "user_createtime", user.getUser_createtime());
+
+    }
+
+    @Override
+    public User findById(Integer user_id) {
+
+        return userMapper.findById(user_id);
+    }
+
+    @Override
+    public User findByTelephone(String user_telephone) {
+
+        //redis中查找
+        if (redisUtil.hashKey(user_telephone)) {
+            Map<Object, Object> hmget = redisUtil.hmget(user_telephone);
+            User user = new User();
+            user.setUser_id((int) hmget.get("user_id"))
+                    .setUser_telephone((String) hmget.get("user_telephone"))
+                    .setUser_password((String) hmget.get("user_password"))
+                    .setUser_faceinfo((String) hmget.get("user_faceinfo"))
+                    .setUser_username((String) hmget.get("user_username"))
+                    .setUser_createtime((Date) hmget.get("user_createtime"));
+            return user;
+        } else {
+            return userMapper.findByTelephone(user_telephone);
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userMapper.findAll();
+    }
+
+    @Override
+    public User showInformation(String user_telephone) {
+
+        //redis中查找
+        if (redisUtil.hashKey(user_telephone)) {
+            Map<Object, Object> hmget = redisUtil.hmget(user_telephone);
+            User user = new User();
+            user.setUser_id((int) hmget.get("user_id"))
+                    .setUser_telephone((String) hmget.get("user_telephone"))
+                    .setUser_password((String) hmget.get("user_password"))
+                    .setUser_faceinfo((String) hmget.get("user_faceinfo"))
+                    .setUser_username((String) hmget.get("user_username"))
+                    .setUser_createtime((Date) hmget.get("user_createtime"));
+            return user;
+        } else {
+            return userMapper.showInformation(user_telephone);
+        }
+    }
 }
