d6b109a551808748cc7739922b80e40bd30aa511
chendengfeng0820 <18790120405@163.com>
2020-06-19 16:52:32 +0800

    springcloud eureka

--- register-login/src/main/java/com/info/register_login/RegisterLoginApplication.java
+++ register-login/src/main/java/com/info/register_login/RegisterLoginApplication.java
@@ -2,8 +2,13 @@ package com.info.register_login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient //服务发现
+
 public class RegisterLoginApplication {
 
     public static void main(String[] args) {
